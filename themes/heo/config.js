ea0bc429601edb2abf94b6be0ec8c736dbedc7d7
JOYADSEN <hainasjoy@gmail.com>
2024-12-20 19:12:12 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: 'Just take a stroll',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'Top Picks', url: '/tag/Top Picks' },
-  HEO_HERO_CATEGORY_2: { title: 'Finance Trends', url: '/tag/Finance Trends' },
-  HEO_HERO_CATEGORY_3: { title: 'Quick Guides', url: '/tag/Quick Guides' },
+  HEO_HERO_CATEGORY_1: { title: 'Top Picks', url: '/tag/Top-Picks' },
+  HEO_HERO_CATEGORY_2: { title: 'Finance Trends', url: '/tag/Finance-Trends' },
+  HEO_HERO_CATEGORY_3: { title: 'Quick Guides', url: '/tag/Quick-Guides' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
